8b20eed8a59238d7d32f411b0a318d4643f8b000
Gabriel Rozendo <79455923+rozendox@users.noreply.github.com>
2023-03-10 02:48:06 -0300

    Create Funcoes_Anonimas_Lambda.py

---
+++ Funcoes_Anonimas_Lambda.py
@@ -0,0 +1,10 @@
+"""
+
+função anônima que calcula o quadrado de um número usando lambda. 
+Estamos atribuindo a função a uma variável chamada quadrado e chamando-a com um argumento de 5 para mostrar que ela funciona corretamente.
+
+"""
+
+
+quadrado = lambda x: x ** 2
+print(quadrado(5))
